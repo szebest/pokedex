252cd896636a4c9622c31e5d98c47bfe57c62c2c
Mateusz Szebestik <szebestik@gmail.com>
2024-04-06 21:58:51 +0200

    Changed the breakpoint, capitalized the stats name

--- src/app/modules/pokemon/components/stats-tab/stats-tab.component.ts
+++ src/app/modules/pokemon/components/stats-tab/stats-tab.component.ts
@@ -1,10 +1,12 @@
 import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { SinglePokemonResponse } from '../../models';
 
 @Component({
   selector: 'app-stats-tab',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stats-tab.component.html',
   styleUrl: './stats-tab.component.scss',
 })
